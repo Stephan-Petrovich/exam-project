d1cc62640321b439f57f01ec98d57b4810d12ae7
Stephan-Petrovich <ribinstepan10@mail.ru>
2025-10-27 11:51:38 +0300

    first state of project

---
+++ src/components/Application/index.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from "react";
+
+const App = (): ReactElement => {
+    return <div className="app-block"></div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import App from "./components/Application";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(<App />);
